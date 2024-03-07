9120b022210b540e0d9b9c9c563eb8a084869280
Aleks37 <lao35180972643335589007>
2024-03-07 13:06:12 +0400

    Initial Commit

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Уменьшаем сложность (О-Нотацию) алгоритма:
+int Promt(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine());
+}
+int[,] SymmetricMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++){
+        for (int j = i; j < matrix.GetLength(1); j++){  // "j = i" - уменьшаем кол. прогонов, а соот и сложность.
+            matrix[i, j] = (i + 1) * (j + 1);
+            matrix[j, i] = (i + 1) * (j + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++){
+        for (int j = 0; j < matrix.GetLength(1); j++){
+            Console.Write($"{matrix[i, j]:f1}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = Promt("Введите число N (длина таблицы): ");
+int[,] matrix = new int[n, n];
+int[,] resultMatrix = SymmetricMatrix(matrix);
+PrintMatrix(resultMatrix);
